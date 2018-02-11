b33fa66f15d5dacbd6b4299c47b39a39ff121670
Jing <tangj90@gmail.com>
2018-02-11 12:34:09 +0800

    Upload executable file.

---
+++ sample-code.bat
@@ -0,0 +1,9 @@
+REM Begin
+
+SET exec=InfMaxHop1.0.exe
+CALL %exec% -func=0 -gname=facebook
+PAUSE
+CALL %exec% -func=1 -gname=facebook -alg=oneHop -seedsize=50
+CALL %exec% -func=1 -gname=facebook -alg=twoHop -seedsize=100 -model=LT -pdist=WC
+CALL %exec% -func=1 -gname=facebook -alg=twoHop -opt=0 -seedsize=100 -model=IC -pdist=TRI
+PAUSE
